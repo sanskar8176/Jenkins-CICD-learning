fd5ef54a5ecd1188bdc37103a9c7848d273dd8f1
Sanskar Singh <76558215+sanskar8176@users.noreply.github.com>
2024-07-11 19:32:07 +0530

    minor changes

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program");
-        System.out.println("Output of java program");
+        System.out.println("This is output of java program");
         System.out.println(new Date());
         
 } 
